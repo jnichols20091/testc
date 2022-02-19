23e2e37f520618af5266f889c8420b563215cd45
josh <jnichols20091@gmail.com>
2022-02-18 23:30:05 -0600

    q

---
+++ chp4/tracer.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+struct Tracer {
+  Tracer(const char* name) : name{ name } {
+    printf("%s constructed.\n", name); 
+  }
+  ~Tracer() {
+    printf("%s destructed.\n", name); 
+  }
+private:
+  const char* const name;
+};
